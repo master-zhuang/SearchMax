c64ba0652df9fc8baded022978284ce658bee0ed
zhuangxk <zhuangxuekun@limei.com>
2016-01-13 18:26:44 +0800

    ReactPaginate  module found

--- backend/handler/web/assets/js/search.js
+++ backend/handler/web/assets/js/search.js
@@ -103,7 +103,6 @@ var SearchBanner = React.createClass({
                             </button>
                         </span>
                     </div>
-
                 </div>
             </div>
           </div>
@@ -305,5 +304,145 @@ ReactDOM.render(
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var URL = 'http://developer.echonest.com/api/v4/song/search?api_key=JE2S42FJUGYGJFVSE';
+var PER_PAGE = 2;
+
+var Paginator = React.createClass({
+    propTypes: {
+        max: React.PropTypes.number.isRequired,
+        maxVisible: React.PropTypes.number,
+        onChange: React.PropTypes.func.isRequired
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        if (prevState.currentPage !== this.state.currentPage) {
+            this.props.onChange(this.state.currentPage);
+        }
+    },
+    getDefaultProps: function() {
+        return {
+            maxVisible: 5
+        };
+    },
+    getInitialState: function() {
+        return {
+            currentPage: 1,
+            items: []
+        };
+    },
+    goTo: function(page) {
+        this.setState({currentPage: page});
+    },
+
+    onClickNext: function() {
+        var page = this.state.currentPage;
+
+        if (page < this.props.max) {
+            this.goTo(page + 1);
+        }
+    },
+    onClickPrev: function() {
+        if (this.state.currentPage > 1) {
+            this.goTo(this.state.currentPage - 1);
+        }
+    },
+    render: function() {
+        var className = this.props.className || '',
+            p = this.props,
+            s = this.state,
+            skip = 0;
+
+        if (s.currentPage > p.maxVisible - 1 && s.currentPage < p.max) {
+            skip = s.currentPage - p.maxVisible + 1;
+        } else if (s.currentPage === p.max) {
+            skip = s.currentPage - p.maxVisible;
+        }
+
+        var iterator = Array.apply(null, Array(p.maxVisible)).map(function(v, i) {
+            return skip + i + 1;
+        });
+
+        return (
+            <nav>
+                <ul className={'pagination ' + className}>
+                    <li className={s.currentPage === 1 ? 'disabled' : ''}>
+                        <a href="#" onClick={this.onClickPrev}>
+                            <span aria-hidden="true">&laquo;</span>
+                            <span className="sr-only">Prev</span>
+                        </a>
+                    </li>
+                    {iterator.map(function(page) {
+                        return (
+                            <li key={page}
+                                onClick={this.goTo.bind(this, page)}
+                                className={s.currentPage === page ? 'active' : ''}>
+                                <a href="#">{page}</a>
+                            </li>
+                        );
+                    }, this)}
+                    <li className={s.currentPage === p.max ? 'disabled' : ''}>
+                        <a href="#" onClick={this.onClickNext}>
+                            <span aria-hidden="true">&raquo;</span>
+                            <span className="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+});
+
+var App = React.createClass({
+    componentDidMount: function() {
+        this.onChangePage(1);
+    },
+    getData: function(page) {
+        return $.getJSON(URL, {
+            artist: 'the postal service',
+            results: PER_PAGE,
+            start: PER_PAGE * (page - 1)
+        }).then(function(result) {
+            return result.response.songs;
+        });
+    },
+    getInitialState: function() {
+        return {
+            items: [],
+            loading: true
+        };
+    },
+    onChangePage: function(page) {
+        this.setState({
+            loading: true
+        });
+
+        this.getData(page).then(function(items) {
+            this.setState({
+                items: items,
+                loading: false
+            });
+        }.bind(this));
+    },
+    renderItem: function(item) {
+        return <li key={item.id}>{item.title}</li>;
+    },
+    render: function() {
+        var s = this.state;
+
+        return (
+            <div>
+                <h1>Paginator example</h1>
+                <Paginator max={10} onChange={this.onChangePage} />
+                {s.loading
+                    ? <div>Loading...</div>
+                    : <ul>{s.items.map(this.renderItem)}</ul>
+                }
+            </div>
+        );
+    }
+});
+
+React.render(<App />, document.getElementById('search-pages'));
 
 
